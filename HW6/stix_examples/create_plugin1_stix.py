76d0e4c070a56bc5dcb072b1867ebcc2dc56938b
michael5486 <michael5486@gmail.com>
2018-05-03 16:37:34 -0400

    finiashed code?

--- HW6/stix_examples/create_plugin1_stix.py
+++ HW6/stix_examples/create_plugin1_stix.py
@@ -3,10 +3,9 @@ from stix.indicator import Indicator
 from stix.ttp import TTP, Behavior
 from stix.ttp.behavior import MalwareInstance
 from cybox.core import Observable
+from cybox.common import Hash
 from cybox.objects.file_object import File
 from cybox.objects.win_registry_key_object import WinRegistryKey
-from cybox.objects.network_packet_object import NetworkPacket, IPv4Packet, IPv4Header
-from cybox.common import Hash
 from cybox.objects.address_object import Address
 
 def main():
@@ -14,7 +13,7 @@ def main():
 
     malware_instance = MalwareInstance()
     malware_instance.add_name("plugin1.exe")
-    #not really remote access but idk what else to put
+    #not really remote access but am not sure what else to put
     malware_instance.add_type("Remote Access Trojan")
 
     ttp = TTP(title="Install+plugin1.exe")
@@ -32,7 +31,7 @@ def main():
     file_plugin1 = File()
     file_plugin1.file_name = "plugin1.exe"
     file_plugin1.file_path = "C:\\Users\\Default\\AppData\\Local\\temp\plugin1"
-    file_plugin1.add_hash(Hash("insert Hash here"))
+    file_plugin1.add_hash(Hash("ae768b62f5fef4dd604e1b736bdbc3ed30417ef4f67bff74bb57f779d794d6df"))
     file_plugin1.hashes[0].simple_hash_value.condition = "Equals"
 
     #observable 3 registry key
@@ -41,18 +40,9 @@ def main():
     registry_object.name = "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run\\Google Ultron Updater"
 
     #observable 4 network traffic
-    #http://cybox.readthedocs.io/en/stable/api/cybox/objects/network_packet_object.html#cybox.objects.network_packet_object.IPv4Header
-    #http://cybox.readthedocs.io/en/stable/api/cybox/objects/address_object.html#cybox.objects.address_object.Address
-    src_address = Address("192.168.52.2")
-    dest_address = Address("192.168.52.2")
-    nbns_ipv4_header = IPv4Header()
-    nbns_ipv4_header.src_ipv4_addr = src_address
-    nbns_ipv4_header.dest_ipv4_addr = dest_address
-    #nbns.protocol = "NBNS"
-
-    network_nbns_packet = IPv4Packet()
-    network_nbns_packet.ipv4_header = nbns_ipv4_header
-
+    #http://stixproject.github.io/documentation/idioms/malware-hash/
+    #I couldn't figure out how to correctly indicate the source, dest or protocol
+    addr = Address(address_value="192.168.52.219", category=Address.CAT_IPV4)
 
     #indicator 1 Install+plugin1.exe
     indicator = Indicator(title="File hash for Install+plugin.exe")
@@ -72,22 +62,19 @@ def main():
     indicator3.add_observable(registry_object)
     indicator3.add_indicated_ttp(TTP(idref=ttp.id_))
 
-    #indicator4 NBNS packet
-    #use this to find sample code
-    indicator4 = Indicator(title="NetBios network traffic")
-    indicator4.add_indicator_type("IP Watchlist")
-    indicator4.add_observable(nbns_ipv4_header)
+    #indicator4 network traffic
+    indicator4 = Indicator(title="Network Traffic for plugine1.exe")
+    indicator.add_indicator_type("IP Watchlist")
+    indicator4.add_observable(Observable(addr))
     indicator4.add_indicated_ttp(TTP(idref=ttp.id_))
 
     stix_package.add_indicator(indicator)
     stix_package.add_indicator(indicator2)
     stix_package.add_indicator(indicator3)
+    stix_package.add_indicator(indicator4)
     stix_package.add_ttp(ttp)
 
     print(stix_package.to_xml(encoding=None))
 
 if __name__ == '__main__':
     main()
-
-
-
