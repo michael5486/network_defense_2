1df5b81a1c0653a5e88b83673ac981702b867508
michael5486 <michael5486@gmail.com>
2018-05-07 12:21:18 -0400

    coa for python added

---
+++ HW6/stix_examples/stix_tor_coa.py
@@ -0,0 +1,52 @@
+import re
+from stix.coa import CourseOfAction, Objective
+from stix.common import Confidence
+from stix.core import STIXPackage
+from cybox.core import Observables
+from cybox.objects.address_object import Address
+
+#used this file
+#https://collector.torproject.org/recent/exit-lists/2018-05-04-19-02-00
+
+def main():
+
+    fileIn = open('tor_exit_node_list.txt', 'r')
+    fileOut = open('coa_tor.xml', 'w')
+
+    #print("List of Tor Exit nodes as of 5/4/2018")
+    ip_addr_list = []
+
+    for line in fileIn:
+
+        ip_addr = re.search('(([2][5][0-5]\.)|([2][0-4][0-9]\.)|([0-1]?[0-9]?[0-9]\.)){3}(([2][5][0-5])|([2][0-4][0-9])|([0-1]?[0-9]?[0-9]))', line)
+        if ip_addr:
+            ip_addr_list.append(ip_addr)
+            #print("    ", ip_addr.group(0))
+
+    pkg = STIXPackage()
+
+    coa = CourseOfAction()
+    coa.title = "Block traffic to Tor exit nodes"
+    coa.stage = "Response"
+    coa.type_ = "Perimeter Blocking"
+
+    obj = Objective()
+    obj.description = "Block communication to Tor exit nodes"
+    obj.applicability_confidence = Confidence("High")
+
+    i = 0
+    for ip_addr in ip_addr_list:
+
+        addr = Address(address_value="ip_addr", category=Address.CAT_IPV4)
+        coa.parameter_observables = Observables(addr)
+
+        pkg.add_course_of_action(coa)
+        print(i)
+        i = i + 1
+
+    fileOut.write(pkg.to_xml(encoding=None))
+
+if __name__ == '__main__':
+    main()
+
+
